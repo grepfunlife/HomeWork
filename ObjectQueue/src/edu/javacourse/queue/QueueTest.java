081fc7b054fe759fc8211b0af2440338acec3771
grepfunlife <love_polyanskaya@MacBook-Air-Lubov.local>
2015-11-09 23:34:04 +0300

    gitignor

---
+++ ObjectQueue/src/edu/javacourse/queue/QueueTest.java
@@ -0,0 +1,25 @@
+package edu.javacourse.queue;
+
+public class QueueTest
+{
+    public static void main(String[] arg) {
+        ObjectQueue queue = new ObjectQueue();
+
+        for(int i=0; i<10; i++) {
+            // В данном случае мы складываем в очередь строки
+            queue.push("Строка:" + i);
+        }
+
+        for(int i=0; i<queue.size(); i++) {
+            String s = (String)queue.get(i);
+            System.out.println(s);
+        }
+        System.out.println("===============");
+        while(queue.size() > 0) {
+            // Мы делаем жесткое приведение типа, т.к. знаем, что там лежат строки
+            String s = (String)queue.pull();
+            System.out.println(s);
+            System.out.println("Размер очереди:" + queue.size());
+        }
+    }
+}
